99969e277f2cbddcfc16acd9f7c58e2a27d110cb
tharunrajravi <tharunrajravi440@gmail.com>
2026-01-28 11:18:48 +0530

    Initial flask app

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Deployed using Docker + GitHub Actions on AWS EC2"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
